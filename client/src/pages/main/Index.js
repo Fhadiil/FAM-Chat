8af1da77858ff06feef20f77025bb16e9f07864e
Fhadiil <fadeelaliyu51@gmail.com>
2024-04-19 12:21:46 +0100

    setup

--- client/src/pages/main/Index.js
+++ client/src/pages/main/Index.js
@@ -1,13 +1,7 @@
 import React from "react";
-import CustomCard from "../../components/custom/CustomCard";
-import { Link } from "react-router-dom";
-import { Col, Row } from "reactstrap";
-import { RiSecurePaymentLine } from "react-icons/ri";
-import { VscWorkspaceTrusted } from "react-icons/vsc";
-import { FaRunning } from "react-icons/fa";
 
 const Index = () => {
-  return <>CHAATTTTTT!!!!!!!!</>;
+  return <h1>CHAATTTTTT!!!!!!!!</h1>;
 };
 
 export default Index;
